90f266939a219db0e8c371010d2cc49ed9f353dd
Jacob Reimers <jacob@reimers.dk>
2025-07-28 06:12:59 +0200

    Code cleanup

--- tests/features/steps/message_handling.py
+++ tests/features/steps/message_handling.py
@@ -7,26 +7,20 @@ from aett.eventstore import Topic
 from behave import given, when, then, step, use_step_matcher
 from testcontainers.rabbitmq import RabbitMqContainer
 
+from features.steps.test_types import SubTestEvent, OtherTestEvent
 from sirabus import generate_vhost_name
 from sirabus.servicebus.cloudevent_servicebus import (
     create_servicebus_for_amqp_cloudevent,
     create_servicebus_for_memory_cloudevent,
 )
-from sirabus.message_pump import MessagePump
+from sirabus.servicebus.inmemory_servicebus import MessagePump
 from sirabus.topography import TopographyBuilder
 from sirabus.publisher.cloudevent_publisher import (
     create_publisher_for_amqp_cloudevent,
     create_publisher_for_memory_cloudevent,
 )
 from sirabus.hierarchical_topicmap import HierarchicalTopicMap
-from tests.features.steps.test_types import (
-    TestEvent,
-    TestEventHandler,
-    SubTestEvent,
-    OtherTestEvent,
-    NestedTestEvent,
-    OtherTestEventHandler,
-)
+from tests.features.steps.test_types import TestEvent, TestEventHandler
 
 use_step_matcher("re")
 
@@ -35,7 +29,6 @@ use_step_matcher("re")
 def step_impl1(context):
     logging.basicConfig(level=logging.DEBUG)
     context.wait_handle = threading.Event()
-    context.wait_handle2 = threading.Event()
     context.topic_map = HierarchicalTopicMap()
     container = RabbitMqContainer(vhost=generate_vhost_name("test", "0.0.0"))
     container.start()
@@ -46,10 +39,7 @@ def step_impl1(context):
         "%2F" if params.virtual_host == "/" else params.virtual_host.strip("/")
     )
     context.connection_string = f"amqp://{creds.username}:{creds.password}@{params.host}:{params.port}/{virtual_host}"
-    context.handlers = [
-        TestEventHandler(wait_handle=context.wait_handle),
-        OtherTestEventHandler(wait_handle=context.wait_handle2),
-    ]
+    context.handlers = [TestEventHandler(wait_handle=context.wait_handle)]
 
 
 @given("a running in-memory message broker")
@@ -64,8 +54,8 @@ def step_impl(context):
 def step_impl2(context):
     context.topic_map.add(Topic.get(TestEvent), TestEvent)
     context.topic_map.add(Topic.get(SubTestEvent), SubTestEvent)
+    context.topic_map.add(Topic.get(SubTestEvent), SubTestEvent)
     context.topic_map.add(Topic.get(OtherTestEvent), OtherTestEvent)
-    context.topic_map.add(Topic.get(NestedTestEvent), NestedTestEvent)
 
 
 @step("amqp broker is configured with the hierarchical topic map")
@@ -99,10 +89,9 @@ def step_impl4(context):
     logging.debug("Topography built.")
 
 
-@when("I send a (?P<topic>.+) message to the amqp service bus")
-def step_impl5(context, topic):
-    event_type = context.topic_map.resolve_type(topic)
-    event = event_type(
+@when("I send a message to the amqp service bus")
+def step_impl5(context):
+    event = TestEvent(
         source="test",
         timestamp=datetime.datetime.now(datetime.timezone.utc),
         correlation_id=str(uuid.uuid4()),
@@ -113,10 +102,9 @@ def step_impl5(context, topic):
     context.async_runner.run_async(publisher.publish(event))
 
 
-@when("I send a (?P<topic>.+) message to the in-memory service bus")
-def step_impl6(context, topic):
-    event_type = context.topic_map.resolve_type(topic)
-    event = event_type(
+@when("I send a message to the in-memory service bus")
+def step_impl6(context):
+    event = TestEvent(
         source="test",
         timestamp=datetime.datetime.now(datetime.timezone.utc),
         correlation_id=str(uuid.uuid4()),
@@ -130,21 +118,7 @@ def step_impl6(context, topic):
 @then("the message is received by the subscriber")
 def step_impl7(context):
     try:
-        result = context.wait_handle.wait(timeout=5)
+        result = context.wait_handle.wait(timeout=15)
         assert result, "The message was not received by the subscriber in time"
     finally:
         context.async_runner.run_async(context.consumer.stop())
-
-
-@step("the other event handlers are not invoked")
-def step_impl8(context):
-    assert context.wait_handle2.is_set() is False, (
-        "The other event handler was invoked, but it should not have been"
-    )
-
-
-@then("the messages are received by the subscriber")
-def step_impl9(context):
-    assert context.wait_handle2.is_set() and context.wait_handle.is_set(), (
-        "The message was not received by the subscriber in time"
-    )
