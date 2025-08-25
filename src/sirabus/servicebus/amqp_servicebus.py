5d131b5433ccea46fb1252346d1f2a72b90dd008
Jacob Reimers <jacob@reimers.dk>
2025-08-25 17:37:59 +0200

    Alter service bus creation API to use configuration classes.

--- src/sirabus/servicebus/amqp_servicebus.py
+++ src/sirabus/servicebus/amqp_servicebus.py
@@ -1,8 +1,7 @@
-import logging
-from typing import List, Optional, Set, Callable, Tuple
+from typing import Optional, Callable, Tuple
 
 import aio_pika
-from aett.eventstore import BaseEvent
+from aett.eventstore import BaseEvent, BaseCommand
 from aio_pika.abc import (
     AbstractIncomingMessage,
     AbstractRobustConnection,
@@ -11,102 +10,116 @@ from aio_pika.abc import (
 
 from sirabus import IHandleEvents, IHandleCommands, CommandResponse, get_type_param
 from sirabus.hierarchical_topicmap import HierarchicalTopicMap
-from sirabus.servicebus import ServiceBus
+from sirabus.servicebus import ServiceBus, ServiceBusConfiguration
 
 
-class AmqpServiceBus(ServiceBus):
+class AmqpServiceBusConfiguration(ServiceBusConfiguration):
+    def __init__(
+        self,
+        message_reader: Callable[
+            [HierarchicalTopicMap, dict, bytes], Tuple[dict, BaseEvent | BaseCommand]
+        ],
+        command_response_writer: Callable[[CommandResponse], Tuple[str, bytes]],
+    ):
+        super().__init__(
+            message_reader=message_reader,
+            command_response_writer=command_response_writer,
+        )
+        self._amqp_url: Optional[str] = None
+        self._prefetch_count: int = 10
+        import uuid
+
+        self._receive_endpoint_name: str = str(uuid.uuid4())
+
+    def get_amqp_url(self) -> Optional[str]:
+        return self._amqp_url
+
+    def get_prefetch_count(self) -> int:
+        return self._prefetch_count
+
+    def get_receive_endpoint_name(self) -> str:
+        return self._receive_endpoint_name
+
+    def with_amqp_url(self, amqp_url: str):
+        self._amqp_url = amqp_url
+        return self
+
+    def with_prefetch_count(self, prefetch_count: int):
+        if prefetch_count < 1:
+            raise ValueError("prefetch_count must be >= 1")
+        self._prefetch_count = prefetch_count
+        return self
+
+    def with_receive_endpoint_name(self, receive_endpoint_name: str):
+        if not receive_endpoint_name or receive_endpoint_name == "":
+            raise ValueError("receive_endpoint_name must not be empty")
+        self._receive_endpoint_name = receive_endpoint_name
+        return self
+
+    def build(self):
+        if not self.get_amqp_url:
+            raise ValueError("AMQP URL must be set before building the service bus.")
+        return AmqpServiceBus(configuration=self)
+
+    @staticmethod
+    def default():
+        from sirabus.publisher.pydantic_serialization import (
+            read_event_message,
+            create_command_response,
+        )
+
+        return AmqpServiceBusConfiguration(
+            message_reader=read_event_message,
+            command_response_writer=create_command_response,
+        )
+
+    @staticmethod
+    def for_cloud_event():
+        from sirabus.publisher.cloudevent_serialization import (
+            read_cloudevent_message,
+            create_command_response,
+        )
+
+        return AmqpServiceBusConfiguration(
+            message_reader=read_cloudevent_message,
+            command_response_writer=create_command_response,
+        )
+
+
+class AmqpServiceBus(ServiceBus[AmqpServiceBusConfiguration]):
     """
     An implementation of the ServiceBus that uses AMQP (Advanced Message Queuing Protocol)
     for communication with RabbitMQ. This class is designed to handle both events and commands
     using a hierarchical topic map for routing messages.
     It supports asynchronous message handling and allows for command responses to be sent back
     to the requester.
-    :param amqp_url: The AMQP URL to connect to RabbitMQ.
-    :param topic_map: The topic map to use for resolving topics.
-    :param handlers: The list of event and command handlers to register.
-    :param message_reader: A callable that reads messages from the topic map and returns a tuple
-                         containing the headers and the BaseEvent instance.
-    :param command_response_writer: A callable that writes command responses to the appropriate topic.
-    :param prefetch_count: The number of messages to prefetch from RabbitMQ.
-    :param logger: An optional logger instance for logging messages.
-    :type amqp_url: str
-    :type topic_map: HierarchicalTopicMap
-    :type handlers: List[IHandleEvents | IHandleCommands]
-    :type message_reader: Callable[[HierarchicalTopicMap, dict, bytes], Tuple[dict, BaseEvent]]
-    :type command_response_writer: Callable[[CommandResponse], Tuple[str, bytes]]
-    :type prefetch_count: int
-    :type logger: Optional[logging.Logger]
-    :raises ValueError: If the AMQP URL is invalid or if no handlers are provided.
-    :raises RuntimeError: If the connection to RabbitMQ cannot be established.
-    :raises Exception: For any other exceptions that occur during message handling.
     :note: This class is designed to be used in an asynchronous context, and it requires
            an event loop to run the `run` method. The `stop` method should be called to
            gracefully shut down the service bus and close the connection to RabbitMQ.
-    :example:
-        >>> from sirabus import AmqpServiceBus, HierarchicalTopicMap, IHandleEvents
-        >>> import asyncio
-        >>> class MyEventHandler(IHandleEvents):
-        ...     async def handle(self, event, headers):
-        ...         print(f"Handling event: {event} with headers: {headers}")
-        >>> topic_map = HierarchicalTopicMap()
-        >>> handlers = [MyEventHandler()]
-        >>> amqp_url = "amqp://guest:guest@localhost/"
-        >>> message_reader = lambda topic_map, headers, body: (headers, BaseEvent.from_json(body))
-        >>> command_response_writer = lambda response: ("response_topic", response.to_json().encode('utf-8'))
-        >>> service_bus = AmqpServiceBus(
-        ...     amqp_url=amqp_url,
-        ...     topic_map=topic_map,
-        ...     handlers=handlers,
-        ...     message_reader=message_reader,
-        ...     command_response_writer=command_response_writer,
-        ...     prefetch_count=10,
-        ... )
-        >>> asyncio.run(service_bus.run())
     :note: The `run` method starts the service bus and begins consuming messages from RabbitMQ.
            The `stop` method should be called to gracefully shut down the service bus and close
            the connection to RabbitMQ.
     """
 
-    def __init__(
-        self,
-        amqp_url: str,
-        topic_map: HierarchicalTopicMap,
-        handlers: List[IHandleEvents | IHandleCommands],
-        message_reader: Callable[
-            [HierarchicalTopicMap, dict, bytes], Tuple[dict, BaseEvent]
-        ],
-        command_response_writer: Callable[[CommandResponse], Tuple[str, bytes]],
-        prefetch_count: int = 10,
-        logger: Optional[logging.Logger] = None,
-    ) -> None:
+    def __init__(self, configuration: AmqpServiceBusConfiguration) -> None:
         """Create a new instance of the consumer class, passing in the AMQP
         URL used to connect to RabbitMQ.
-        :param str amqp_url: The AMQP URL to connect to RabbitMQ.
-        :param HierarchicalTopicMap topic_map: The topic map to use for resolving topics.
-        :param List[IHandleEvents] handlers: The list of event handlers to register.
-        :param int prefetch_count: The number of messages to prefetch from RabbitMQ.
+        :param AmqpServiceBusConfiguration configuration: The AMQP service bus configuration.
         """
         super().__init__(
-            message_reader=message_reader,
-            topic_map=topic_map,
-            handlers=handlers,
-            logger=logger or logging.getLogger("AmqpServiceBus"),
+            configuration=configuration,
         )
-        self.__command_response_writer = command_response_writer
-        self.__amqp_url = amqp_url
-        self._topic_map = topic_map
-        self._prefetch_count = prefetch_count
-        self._logger = logger or logging.getLogger("ServiceBus")
         self.__topics = set(
             topic
             for topic in (
-                self._topic_map.get_from_type(get_type_param(handler))
-                for handler in handlers
+                self._configuration.get_topic_map().get_from_type(
+                    get_type_param(handler)
+                )
+                for handler in configuration.get_handlers()
                 if isinstance(handler, (IHandleEvents, IHandleCommands))
             )
             if topic is not None
         )
-        self.__queue_name = self._get_consumer_queue_name(self.__topics)
         self.__connection: Optional[AbstractRobustConnection] = None
         self.__channel: Optional[AbstractRobustChannel] = None
         self.__consumer_tag: Optional[str] = None
@@ -122,29 +135,41 @@ class AmqpServiceBus(ServiceBus):
             )
             await msg.ack()
         except Exception as e:
-            self._logger.exception("Exception while handling message", exc_info=e)
+            self._configuration.get_logger().exception(
+                "Exception while handling message", exc_info=e
+            )
             await msg.nack(requeue=True)
 
     async def run(self):
-        self._logger.debug("Starting service bus")
-        self.__connection = await aio_pika.connect_robust(url=self.__amqp_url)
+        self._configuration.get_logger().debug("Starting service bus")
+        self.__connection = await aio_pika.connect_robust(
+            url=self._configuration.get_amqp_url()
+        )
         self.__channel = await self.__connection.channel()
-        await self.__channel.set_qos(prefetch_count=self._prefetch_count)
-        self._logger.debug("Channel opened for consuming messages.")
-        queue = await self.__channel.declare_queue(self.__queue_name, exclusive=True)
+        await self.__channel.set_qos(
+            prefetch_count=self._configuration.get_prefetch_count()
+        )
+        self._configuration.get_logger().debug("Channel opened for consuming messages.")
+        queue = await self.__channel.declare_queue(
+            self._configuration.get_receive_endpoint_name(), exclusive=True
+        )
         for topic in self.__topics:
             await queue.bind(exchange=topic, routing_key=f"{topic}.#")
-            self._logger.debug(f"Queue {self.__queue_name} bound to topic {topic}.")
+            self._configuration.get_logger().debug(
+                f"Queue {self._configuration.get_receive_endpoint_name} bound to topic {topic}."
+            )
         self.__consumer_tag = await queue.consume(callback=self.__inner_handle_message)
 
     async def stop(self):
         if self.__consumer_tag:
-            queue = await self.__channel.get_queue(self.__queue_name)
+            queue = await self.__channel.get_queue(
+                self._configuration.get_receive_endpoint_name()
+            )
             await queue.cancel(self.__consumer_tag)
             await self.__channel.close()
             await self.__connection.close()
 
-    async def send_command_response(
+    async def _send_command_response(
         self,
         response: CommandResponse,
         message_id: str | None,
@@ -153,7 +178,7 @@ class AmqpServiceBus(ServiceBus):
     ):
         if not self.__channel or self.__channel.is_closed:
             return
-        topic, j = self.__command_response_writer(response)
+        topic, j = self._configuration.write_response(response)
         await self.__channel.default_exchange.publish(
             aio_pika.Message(
                 body=j,
@@ -163,21 +188,6 @@ class AmqpServiceBus(ServiceBus):
             ),
             routing_key=reply_to,
         )
-        self._logger.debug(
+        self._configuration.get_logger().debug(
             f"Response published to {reply_to} with correlation_id {correlation_id}."
         )
-
-    @staticmethod
-    def _get_consumer_queue_name(topics: Set[str]) -> str:
-        """
-        Returns the queue name for the given topic.
-        :param topics: The topics for which to get the queue name.
-        :return: The queue name.
-        """
-        import hashlib
-
-        h = hashlib.sha256(usedforsecurity=False)
-        for topic in topics:
-            h.update(topic.encode())
-        hashed_topics = h.hexdigest()
-        return hashed_topics
