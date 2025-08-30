4c989b1f2ad2c9d94447120856b0f8bce07cbf8c
Jacob Reimers <jacob@reimers.dk>
2025-08-30 16:22:29 +0200

    Enable TLS for RabbitMQ.

--- tests/features/steps/message_handling.py
+++ tests/features/steps/message_handling.py
@@ -1,7 +1,10 @@
 import asyncio
 import datetime
 import logging
+import os
+import pathlib
 import uuid
+from ssl import SSLContext, Purpose
 
 from behave import given, when, then, step, use_step_matcher
 from testcontainers.localstack import LocalStackContainer
@@ -10,6 +13,7 @@ from testcontainers.redis import RedisContainer
 
 from sirabus.hierarchical_topicmap import HierarchicalTopicMap
 from sirabus.message_pump import MessagePump
+from sirabus.publisher.amqp_publisher import AmqpPublisherConfiguration
 from sirabus.topography.amqp import TopographyBuilder as AmqpTopographyBuilder
 from sirabus.topography.sqs import TopographyBuilder as SqsTopographyBuilder, SqsConfig
 from tests.features.steps.command_handlers import (
@@ -42,8 +46,11 @@ def generate_vhost_name(name: str, version: str) -> str:
     return h.hexdigest()
 
 
-@given("a running (?P<broker_type>.+) message broker")
-def step_impl1(context, broker_type):
+@given(
+    "a running (?P<broker_type>.+) message broker with TLS (?P<use_tls>enabled|disabled)"
+)
+def step_impl1(context, broker_type, use_tls):
+    use_tls = use_tls == "enabled"
     logging.basicConfig(level=logging.DEBUG)
     context.wait_handle = asyncio.Event()
     context.wait_handle2 = asyncio.Event()
@@ -56,30 +63,45 @@ def step_impl1(context, broker_type):
     ]
     match broker_type:
         case "amqp":
-            set_up_amqp_broker(context)
+            set_up_amqp_broker(context=context, use_tls=use_tls)
         case "SQS":
-            set_up_sqs_broker(context)
+            set_up_sqs_broker(context=context, use_tls=use_tls)
         case "in-memory":
             pass
         case "redis":
-            set_up_redis(context)
+            set_up_redis(context=context, use_tls=use_tls)
         case _:
             raise ValueError(f"Unknown broker type: {broker_type}")
 
 
-def set_up_amqp_broker(context):
+def set_up_amqp_broker(context, use_tls: bool = False):
     """Sets up a RabbitMQ container for testing."""
-    container = RabbitMqContainer(vhost=generate_vhost_name("test", "0.0.0")).start()
+    container = RabbitMqContainer(vhost=generate_vhost_name("test", "0.0.0"))
+    if use_tls:
+        current_dir = pathlib.Path(__file__).resolve().parent
+        container = (
+            container.with_exposed_ports(5671, 5672)
+            .with_volume_mapping(
+                f"{current_dir}/../../configs/rabbitmq.conf",
+                "/etc/rabbitmq/rabbitmq.conf",
+            )
+            .with_volume_mapping(
+                f"{current_dir}/../../configs/certs", "/etc/rabbitmq/certs"
+            )
+        )
+    container = container.start()
+
     context.containers.append(container)
     params = container.get_connection_params()
+
     creds = params.credentials
     virtual_host = (
         "%2F" if params.virtual_host == "/" else params.virtual_host.strip("/")
     )
-    context.connection_string = f"amqp://{creds.username}:{creds.password}@{params.host}:{params.port}/{virtual_host}"
+    context.connection_string = f"{'amqps' if use_tls else 'amqp'}://{creds.username}:{creds.password}@{params.host}:{container.get_exposed_port(5671) if use_tls else params.port}/{virtual_host}"
 
 
-def set_up_sqs_broker(context):
+def set_up_sqs_broker(context, use_tls: bool = False):
     container = (
         LocalStackContainer(image="localstack/localstack:latest")
         .with_services("sns", "sqs")
@@ -99,7 +121,7 @@ def set_up_sqs_broker(context):
     )
 
 
-def set_up_redis(context):
+def set_up_redis(context, use_tls: bool = False):
     container = RedisContainer().start()
     context.connection_string = f"redis://{container.get_container_host_ip()}:{container.get_exposed_port(6379)}"
     context.containers.append(container)
@@ -114,35 +136,62 @@ def step_impl2(context):
 
 
 @step(
-    "a (?P<serializer>.+?) (?P<broker_type>.+) service bus is configured with the hierarchical topic map"
+    "a (?P<serializer>.+?) (?P<broker_type>.+) service bus with TLS (?P<use_tls>enabled|disabled) is configured with the hierarchical topic map"
 )
-async def step_impl3(context, serializer, broker_type):
+async def step_impl3(context, serializer, broker_type, use_tls):
+    use_tls = use_tls == "enabled"
     match (serializer, broker_type):
         case ("cloudevent", "amqp"):
-            await configure_cloudevent_amqp_service_bus(context)
+            await configure_cloudevent_amqp_service_bus(
+                context=context, use_tls=use_tls
+            )
         case ("pydantic", "amqp"):
-            await configure_pydantic_amqp_service_bus(context)
+            await configure_pydantic_amqp_service_bus(context=context, use_tls=use_tls)
         case ("cloudevent", "SQS"):
-            configure_cloudevent_sqs_service_bus(context)
+            configure_cloudevent_sqs_service_bus(context=context, use_tls=use_tls)
         case ("pydantic", "SQS"):
-            configure_pydantic_sqs_service_bus(context)
+            configure_pydantic_sqs_service_bus(context=context, use_tls=use_tls)
         case ("cloudevent", "in-memory"):
-            configure_cloudevent_inmemory_service_bus(context)
+            configure_cloudevent_inmemory_service_bus(context=context, use_tls=use_tls)
         case ("pydantic", "in-memory"):
-            configure_pydantic_inmemory_service_bus(context)
+            configure_pydantic_inmemory_service_bus(context=context, use_tls=use_tls)
         case ("cloudevent", "redis"):
-            configure_cloudevent_redis_service_bus(context)
+            configure_cloudevent_redis_service_bus(context=context, use_tls=use_tls)
         case ("pydantic", "redis"):
-            configure_pydantic_redis_service_bus(context)
+            configure_pydantic_redis_service_bus(context=context, use_tls=use_tls)
 
     await context.consumer.run()
     logging.debug("Topography built.")
 
 
-async def configure_cloudevent_amqp_service_bus(context):
-    builder = AmqpTopographyBuilder(
-        amqp_url=context.connection_string, topic_map=context.topic_map
+def configure_ssl(config, use_tls: bool):
+    if not use_tls:
+        return config
+    import ssl
+
+    certs_path = (
+        pathlib.Path(__file__).parent
+        / ".."
+        / ".."
+        / "configs"
+        / "certs"
+        / "ca_certificate.pem"
     )
+    ssl_context = ssl.create_default_context(
+        purpose=ssl.Purpose.SERVER_AUTH, cafile=certs_path
+    )
+    ssl_context.check_hostname = False
+    return config.with_ssl_config(ssl_context)
+
+
+async def configure_cloudevent_amqp_service_bus(context, use_tls: bool = False):
+    configuration = configure_ssl(
+        AmqpPublisherConfiguration.default()
+        .with_amqp_url(context.connection_string)
+        .with_topic_map(context.topic_map),
+        use_tls,
+    )
+    builder = AmqpTopographyBuilder(configuration=configuration)
     await builder.build()
 
     from sirabus.servicebus.amqp_servicebus import (
@@ -150,19 +199,24 @@ async def configure_cloudevent_amqp_service_bus(context):
         AmqpServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         AmqpServiceBusConfiguration.for_cloud_event()
         .with_topic_map(context.topic_map)
         .with_amqp_url(context.connection_string)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = AmqpServiceBus(config)
 
 
-async def configure_pydantic_amqp_service_bus(context):
-    builder = AmqpTopographyBuilder(
-        amqp_url=context.connection_string, topic_map=context.topic_map
+async def configure_pydantic_amqp_service_bus(context, use_tls: bool = False):
+    configuration = configure_ssl(
+        AmqpPublisherConfiguration.default()
+        .with_amqp_url(context.connection_string)
+        .with_topic_map(context.topic_map),
+        use_tls,
     )
+    builder = AmqpTopographyBuilder(configuration=configuration)
     await builder.build()
 
     from sirabus.servicebus.amqp_servicebus import (
@@ -170,16 +224,17 @@ async def configure_pydantic_amqp_service_bus(context):
         AmqpServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         AmqpServiceBusConfiguration.default()
         .with_amqp_url(context.connection_string)
         .with_topic_map(context.topic_map)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = AmqpServiceBus(config)
 
 
-def configure_cloudevent_sqs_service_bus(context):
+def configure_cloudevent_sqs_service_bus(context, use_tls: bool = False):
     builder = SqsTopographyBuilder(context.topic_map, context.sqs_config)
     builder.build()
 
@@ -188,16 +243,17 @@ def configure_cloudevent_sqs_service_bus(context):
         SqsServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         SqsServiceBusConfiguration.for_cloud_event()
         .with_sqs_config(context.sqs_config)
         .with_topic_map(context.topic_map)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = SqsServiceBus(config)
 
 
-def configure_pydantic_sqs_service_bus(context):
+def configure_pydantic_sqs_service_bus(context, use_tls: bool = False):
     builder = SqsTopographyBuilder(context.topic_map, context.sqs_config)
     builder.build()
 
@@ -206,16 +262,17 @@ def configure_pydantic_sqs_service_bus(context):
         SqsServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         SqsServiceBusConfiguration.default()
         .with_sqs_config(context.sqs_config)
         .with_topic_map(context.topic_map)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = SqsServiceBus(config)
 
 
-def configure_cloudevent_inmemory_service_bus(context):
+def configure_cloudevent_inmemory_service_bus(context, use_tls: bool = False):
     context.message_pump = MessagePump()
     context.message_pump.start()
 
@@ -224,46 +281,49 @@ def configure_cloudevent_inmemory_service_bus(context):
         InMemoryServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         InMemoryConfiguration.for_cloud_event()
         .with_topic_map(context.topic_map)
         .with_handlers(*context.handlers)
-        .with_message_pump(context.message_pump)
+        .with_message_pump(context.message_pump),
+        use_tls=use_tls,
     )
     context.consumer = InMemoryServiceBus(config)
 
 
-def configure_cloudevent_redis_service_bus(context):
+def configure_cloudevent_redis_service_bus(context, use_tls: bool = False):
     from sirabus.servicebus.redis_servicebus import (
         RedisServiceBusConfiguration,
         RedisServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         RedisServiceBusConfiguration.for_cloud_event()
         .with_redis_url(context.connection_string)
         .with_topic_map(context.topic_map)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = RedisServiceBus(config)
 
 
-def configure_pydantic_redis_service_bus(context):
+def configure_pydantic_redis_service_bus(context, use_tls: bool = False):
     from sirabus.servicebus.redis_servicebus import (
         RedisServiceBusConfiguration,
         RedisServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         RedisServiceBusConfiguration.default()
         .with_redis_url(context.connection_string)
         .with_topic_map(context.topic_map)
-        .with_handlers(*context.handlers)
+        .with_handlers(*context.handlers),
+        use_tls=use_tls,
     )
     context.consumer = RedisServiceBus(config)
 
 
-def configure_pydantic_inmemory_service_bus(context):
+def configure_pydantic_inmemory_service_bus(context, use_tls: bool = False):
     context.message_pump = MessagePump()
     context.message_pump.start()
 
@@ -272,19 +332,21 @@ def configure_pydantic_inmemory_service_bus(context):
         InMemoryServiceBus,
     )
 
-    config = (
+    config = configure_ssl(
         InMemoryConfiguration.default()
         .with_topic_map(context.topic_map)
         .with_handlers(*context.handlers)
-        .with_message_pump(context.message_pump)
+        .with_message_pump(context.message_pump),
+        use_tls=use_tls,
     )
     context.consumer = InMemoryServiceBus(config)
 
 
 @when(
-    "I send a (?P<serializer>.+) (?P<topic>.+) message to the (?P<broker_type>.+) service bus"
+    "I send a (?P<serializer>.+) (?P<topic>.+) message to the (?P<broker_type>.+) service bus with TLS (?P<use_tls>enabled|disabled)"
 )
-async def step_impl4(context, serializer, topic, broker_type):
+async def step_impl4(context, serializer, topic, broker_type, use_tls):
+    use_tls = use_tls == "enabled"
     event_type = context.topic_map.get(topic)
     event = event_type(
         source="test",
@@ -294,98 +356,112 @@ async def step_impl4(context, serializer, topic, broker_type):
     publisher = None
     match (serializer, broker_type):
         case ("cloudevent", "amqp"):
-            publisher = create_cloudevent_amqp_publisher(context)
+            publisher = create_cloudevent_amqp_publisher(
+                context=context, use_tls=use_tls
+            )
         case ("pydantic", "amqp"):
-            publisher = create_pydantic_amqp_publisher(context)
+            publisher = create_pydantic_amqp_publisher(context=context, use_tls=use_tls)
         case ("cloudevent", "SQS"):
-            publisher = create_cloudevent_sqs_publisher(context)
+            publisher = create_cloudevent_sqs_publisher(
+                context=context, use_tls=use_tls
+            )
         case ("pydantic", "SQS"):
-            publisher = create_pydantic_sqs_publisher(context)
+            publisher = create_pydantic_sqs_publisher(context=context, use_tls=use_tls)
         case ("cloudevent", "in-memory"):
             publisher = create_cloudevent_inmemory_publisher(context)
         case ("pydantic", "in-memory"):
             publisher = create_pydantic_inmemory_publisher(context)
         case ("cloudevent", "redis"):
-            publisher = create_cloudevent_redis_publisher(context)
+            publisher = create_cloudevent_redis_publisher(
+                context=context, use_tls=use_tls
+            )
         case ("pydantic", "redis"):
-            publisher = create_pydantic_redis_publisher(context)
+            publisher = create_pydantic_redis_publisher(
+                context=context, use_tls=use_tls
+            )
     await publisher.publish(event)
 
 
-def create_cloudevent_amqp_publisher(context):
+def create_cloudevent_amqp_publisher(context, use_tls: bool = False):
     from sirabus.publisher.amqp_publisher import (
         AmqpPublisherConfiguration,
         AmqpPublisher,
     )
 
-    config = (
+    config = configure_ssl(
         AmqpPublisherConfiguration.for_cloud_event()
         .with_amqp_url(context.connection_string)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return AmqpPublisher(config)
 
 
-def create_pydantic_amqp_publisher(context):
+def create_pydantic_amqp_publisher(context, use_tls: bool = False):
     from sirabus.publisher.amqp_publisher import (
         AmqpPublisherConfiguration,
         AmqpPublisher,
     )
 
-    config = (
+    config = configure_ssl(
         AmqpPublisherConfiguration.default()
         .with_amqp_url(context.connection_string)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return AmqpPublisher(config)
 
 
-def create_cloudevent_sqs_publisher(context):
+def create_cloudevent_sqs_publisher(context, use_tls: bool = False):
     from sirabus.publisher.sqs_publisher import SqsPublisherConfiguration, SqsPublisher
 
-    config = (
+    config = configure_ssl(
         SqsPublisherConfiguration.for_cloud_event()
         .with_sqs_config(context.sqs_config)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return SqsPublisher(config)
 
 
-def create_pydantic_sqs_publisher(context):
+def create_pydantic_sqs_publisher(context, use_tls: bool = False):
     from sirabus.publisher.sqs_publisher import SqsPublisherConfiguration, SqsPublisher
 
-    config = (
+    config = configure_ssl(
         SqsPublisherConfiguration.default()
         .with_sqs_config(context.sqs_config)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return SqsPublisher(config)
 
 
-def create_cloudevent_inmemory_publisher(context):
+def create_cloudevent_inmemory_publisher(context, use_tls: bool = False):
     from sirabus.publisher.inmemory_publisher import (
         InMemoryPublisherConfiguration,
         InMemoryPublisher,
     )
 
-    config = (
+    config = configure_ssl(
         InMemoryPublisherConfiguration.for_cloud_event()
         .with_message_pump(context.message_pump)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return InMemoryPublisher(config)
 
 
-def create_cloudevent_redis_publisher(context):
+def create_cloudevent_redis_publisher(context, use_tls: bool = False):
     from sirabus.publisher.redis_publisher import (
         RedisPublisher,
         RedisPublisherConfiguration,
     )
 
-    config = (
+    config = configure_ssl(
         RedisPublisherConfiguration.for_cloud_event()
         .with_redis_url(context.connection_string)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return RedisPublisher(config)
 
@@ -404,16 +480,17 @@ def create_pydantic_inmemory_publisher(context):
     return InMemoryPublisher(config)
 
 
-def create_pydantic_redis_publisher(context):
+def create_pydantic_redis_publisher(context, use_tls: bool = False):
     from sirabus.publisher.redis_publisher import (
         RedisPublisher,
         RedisPublisherConfiguration,
     )
 
-    config = (
+    config = configure_ssl(
         RedisPublisherConfiguration.default()
         .with_redis_url(context.connection_string)
-        .with_topic_map(context.topic_map)
+        .with_topic_map(context.topic_map),
+        use_tls=use_tls,
     )
     return RedisPublisher(config)
 
